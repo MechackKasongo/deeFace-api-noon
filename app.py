531dcffcbb42cf3496af2257380ce4d0f289a084
Mechack Kasongo Kamwanya <110738483+MechackKasongo@users.noreply.github.com>
2025-04-22 12:54:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def recognize_person():
     return jsonify({"message": "Aucune correspondance trouvée"}), 404
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
